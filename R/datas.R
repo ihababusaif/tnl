2501c27be99d4916b4619e35f3e54023be89e0eb
IHAB ABUSAIF <censtat@gmail.com>
2021-10-02 21:19:17 +0300

    add data

---
+++ R/datas.R
@@ -0,0 +1,15 @@
+#' @title  An example for create a description of your data
+#' @description  Data set for testing data
+#'
+#' @docType data
+#'
+#' @format An object of class \code{"data.frame"}
+#' \describe{
+#'  \item{X1}{Data generated from normal}
+#'  \item{X2}{Data generated from uniform}
+#'  \item{X3}{Data generated from normal}
+#' }
+#' @references This data set was artificially created for the overviewR package.
+#' @keywords datasets
+#'
+"datas"
